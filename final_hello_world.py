e3a626ad30f7290a3e655f04b2a709121c4fd293
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-02-17 19:40:13 +0000

    added Final hello world

---
+++ final_hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by: Andrew-Ten-Den
+# Created on: February 2022
+# This is the "Final hello world" program, with proper style
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
+    
